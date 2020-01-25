3159b66b1e5bde0f95e09c8bb0499bb1974d084c
denishoornaert <denis.hoornaert@outlook.com>
2020-01-25 16:46:52 +0100

    Project files

--- rtl/MemorEDF/MemorEDF.srcs/sim_1/new/TB_TDMA.sv
+++ rtl/MemorEDF/MemorEDF.srcs/sim_1/new/TB_TDMA.sv
@@ -24,13 +24,10 @@ module TB_TDMA();
 
     reg           clock;
     reg           reset;
-    reg  [31 : 0] delta0;
-    reg  [31 : 0] delta1;
-    reg  [31 : 0] delta2;
-    reg  [31 : 0] delta3;
+    reg  [31 : 0] delta [4];
     wire [1 : 0]  selection;
     
-    TDMA tdma(clock, reset, delta0, delta1, delta2, delta3, selection);
+    TDMA #(4, 32) tdma(clock, reset, delta, selection);
     
     always
     begin
@@ -43,10 +40,10 @@ module TB_TDMA();
     initial
     begin
         reset <= 1;
-        delta0 <= 15;
-        delta1 <= 15;
-        delta2 <= 15;
-        delta3 <= 15;
+        delta[0] <= 15;
+        delta[1] <= 15;
+        delta[2] <= 15;
+        delta[3] <= 15;
         #10;
         reset <= 0;
         #10;
@@ -124,10 +121,10 @@ module TB_TDMA();
         #30
         // Second test with different reserved time slot sizes
         reset <= 1;
-        delta0 <=  4;
-        delta1 <=  8;
-        delta2 <= 16;
-        delta3 <= 32;
+        delta[0] <=  4;
+        delta[1] <=  8;
+        delta[2] <= 16;
+        delta[3] <= 32;
         #10;
         reset <= 0;
         #10;
