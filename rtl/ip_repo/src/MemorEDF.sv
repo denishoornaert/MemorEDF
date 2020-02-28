d53de9481b60e95a975eb47de21be566b13c4880
denishoornaert <denis.hoornaert@outlook.com>
2020-02-28 11:18:00 +0100

    Introduction of the AXI configuration port and correction of the signals transferred between the sub-modules

--- rtl/ip_repo/src/MemorEDF.sv
+++ rtl/ip_repo/src/MemorEDF.sv
@@ -108,53 +108,53 @@ module MemorEDF #
 //		output wire   [C_S00_AXI_RUSER_WIDTH-1 : 0] s00_axi_ruser,
 		output reg                                  s00_axi_rvalid, //**
 		input wire                                  s00_axi_rready,
-//		// Ports of Axi Slave Bus Interface S01_AXI
-//        input wire                                  s01_axi_aclk,
-//        input wire                                  s01_axi_aresetn,
-//        input wire       [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_awid,
-//        input wire     [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_awaddr,
-//        input wire                          [7 : 0] s01_axi_awlen,
-//        input wire                          [2 : 0] s01_axi_awsize,
-//        input wire                          [1 : 0] s01_axi_awburst,
-//        input wire                                  s01_axi_awlock,
-//        input wire                          [3 : 0] s01_axi_awcache,
-//        input wire                          [2 : 0] s01_axi_awprot,
-//        input wire                          [3 : 0] s01_axi_awqos,
-//        input wire                          [3 : 0] s01_axi_awregion,
+		// Ports of Axi Slave Bus Interface S01_AXI
+        input wire                                  s01_axi_aclk,
+        input wire                                  s01_axi_aresetn,
+        input wire       [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_awid,
+        input wire     [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_awaddr,
+        input wire                          [7 : 0] s01_axi_awlen,
+        input wire                          [2 : 0] s01_axi_awsize,
+        input wire                          [1 : 0] s01_axi_awburst,
+        input wire                                  s01_axi_awlock,
+        input wire                          [3 : 0] s01_axi_awcache,
+        input wire                          [2 : 0] s01_axi_awprot,
+        input wire                          [3 : 0] s01_axi_awqos,
+        input wire                          [3 : 0] s01_axi_awregion,
 //        input wire   [C_S01_AXI_AWUSER_WIDTH-1 : 0] s01_axi_awuser,
-//        input wire                                  s01_axi_awvalid,
-//        output wire                                 s01_axi_awready,
-//        input wire     [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_wdata,
-//        input wire [(C_S01_AXI_DATA_WIDTH/8)-1 : 0] s01_axi_wstrb,
-//        input wire                                  s01_axi_wlast,
+        input wire                                  s01_axi_awvalid,
+        output wire                                 s01_axi_awready,
+        input wire     [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_wdata,
+        input wire [(C_S01_AXI_DATA_WIDTH/8)-1 : 0] s01_axi_wstrb,
+        input wire                                  s01_axi_wlast,
 //        input wire    [C_S01_AXI_WUSER_WIDTH-1 : 0] s01_axi_wuser,
-//        input wire                                  s01_axi_wvalid,
-//        output wire                                 s01_axi_wready,
-//        output wire      [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_bid,
-//        output wire                         [1 : 0] s01_axi_bresp,
+        input wire                                  s01_axi_wvalid,
+        output wire                                 s01_axi_wready,
+        output wire      [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_bid,
+        output wire                         [1 : 0] s01_axi_bresp,
 //        output wire   [C_S01_AXI_BUSER_WIDTH-1 : 0] s01_axi_buser,
-//        output wire                                 s01_axi_bvalid,
-//        input wire                                  s01_axi_bready,
-//        input wire       [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_arid,
-//        input wire     [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_araddr,
-//        input wire                          [7 : 0] s01_axi_arlen,
-//        input wire                          [2 : 0] s01_axi_arsize,
-//        input wire                          [1 : 0] s01_axi_arburst,
-//        input wire                                  s01_axi_arlock,
-//        input wire                          [3 : 0] s01_axi_arcache,
-//        input wire                          [2 : 0] s01_axi_arprot,
-//        input wire                          [3 : 0] s01_axi_arqos,
-//        input wire                          [3 : 0] s01_axi_arregion,
+        output wire                                 s01_axi_bvalid,
+        input wire                                  s01_axi_bready,
+        input wire       [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_arid,
+        input wire     [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_araddr,
+        input wire                          [7 : 0] s01_axi_arlen,
+        input wire                          [2 : 0] s01_axi_arsize,
+        input wire                          [1 : 0] s01_axi_arburst,
+        input wire                                  s01_axi_arlock,
+        input wire                          [3 : 0] s01_axi_arcache,
+        input wire                          [2 : 0] s01_axi_arprot,
+        input wire                          [3 : 0] s01_axi_arqos,
+        input wire                          [3 : 0] s01_axi_arregion,
 //        input wire   [C_S01_AXI_ARUSER_WIDTH-1 : 0] s01_axi_aruser,
-//        input wire                                  s01_axi_arvalid,
-//        output wire                                 s01_axi_arready,
-//        output wire      [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_rid,
-//        output wire    [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_rdata,
-//        output wire                         [1 : 0] s01_axi_rresp,
-//        output wire                                 s01_axi_rlast,
+        input wire                                  s01_axi_arvalid,
+        output wire                                 s01_axi_arready,
+        output wire      [C_S01_AXI_ID_WIDTH-1 : 0] s01_axi_rid,
+        output wire    [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_rdata,
+        output wire                         [1 : 0] s01_axi_rresp,
+        output wire                                 s01_axi_rlast,
 //        output wire   [C_S01_AXI_RUSER_WIDTH-1 : 0] s01_axi_ruser,
-//        output wire                                 s01_axi_rvalid,
-//        input wire                                  s01_axi_rready,
+        output wire                                 s01_axi_rvalid,
+        input wire                                  s01_axi_rready,
 		// Ports of Axi Master Bus Interface M00_AXI
         input wire                                  m00_axi_aclk,
         input wire                                  m00_axi_aresetn,
@@ -207,13 +207,13 @@ module MemorEDF #
 //    reg       [C_M00_AXI_ID_WIDTH-1 : 0] bp_axi_bid;
 //    reg                          [1 : 0] bp_axi_bresp;
 //    wire    [C_M00_AXI_BUSER_WIDTH-1 : 0] bp_axi_buser;
-    reg                                  bp_axi_rvalid;
-    reg                                  bp_axi_bready;
-    reg                                  bp_axi_rready;
-    reg       [C_M00_AXI_ID_WIDTH-1 : 0] bp_axi_rid;
-    reg     [C_M00_AXI_DATA_WIDTH-1 : 0] bp_axi_rdata;
-    reg                          [1 : 0] bp_axi_rresp;
-    reg                                  bp_axi_rlast;
+//    reg                                  bp_axi_rvalid;
+//    reg                                  bp_axi_bready;
+//    reg                                  bp_axi_rready;
+//    reg       [C_M00_AXI_ID_WIDTH-1 : 0] bp_axi_rid;
+//    reg     [C_M00_AXI_DATA_WIDTH-1 : 0] bp_axi_rdata;
+//    reg                          [1 : 0] bp_axi_rresp;
+//    reg                                  bp_axi_rlast;
 //    wire    [C_M00_AXI_RUSER_WIDTH-1 : 0] bp_axi_ruser;
 	
     // Internal routing
@@ -226,25 +226,23 @@ module MemorEDF #
     wire                              nonaxi_to_packetizer_stall;
     
     // TODO TEMPORARY!
-    reg   [$clog2(NUMBER_OF_SCHEDULERS)-1 : 0] scheduling_mode;
-    reg                  [REGISTER_SIZE-1 : 0] scheduler_deadlines [NUMBER_OF_QUEUES];
-    reg                  [REGISTER_SIZE-1 : 0] scheduler_periods [NUMBER_OF_QUEUES];
+//    reg   [$clog2(NUMBER_OF_SCHEDULERS)-1 : 0] scheduling_mode;
+//    reg                  [REGISTER_SIZE-1 : 0] scheduler_deadlines [NUMBER_OF_QUEUES];
+//    reg                  [REGISTER_SIZE-1 : 0] scheduler_periods [NUMBER_OF_QUEUES];
     
-    always @(posedge m00_axi_aclk)
-    begin
-        if(m00_axi_aresetn == 0)
-        begin
-            integer i;
-            for (i = 0; i < NUMBER_OF_QUEUES; i = i + 1)
-            begin
-                scheduler_deadlines[i] <= 0;
-            end
-            for (i = 1; i < NUMBER_OF_QUEUES; i = i + 1)
-            begin
-                scheduler_periods[i] <= 0;
-            end
-            scheduler_periods[0] <= 32; // Arbitrary number
-            scheduling_mode <= 0;
+    // Issued from the configuration port
+    wire          [((C_S01_AXI_DATA_WIDTH/8)*32)-1 : 0] buffers;
+    wire           [$clog2(NUMBER_OF_SCHEDULERS)-1 : 0] scheduling_mode;
+    wire [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] scheduler_deadlines;
+    wire [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] scheduler_periods;
+    assign scheduler_periods = buffers[(1*C_S01_AXI_DATA_WIDTH)-1 : 0];
+    assign scheduler_deadlines = buffers[(2*C_S01_AXI_DATA_WIDTH)-1 : (1*C_S01_AXI_DATA_WIDTH)];
+    assign scheduling_mode = buffers[(2*C_S01_AXI_DATA_WIDTH)+$clog2(NUMBER_OF_SCHEDULERS)-1 : (2*C_S01_AXI_DATA_WIDTH)];
+    
+//    always @(posedge m00_axi_aclk)
+//    begin
+//        if(m00_axi_aresetn == 0)
+//        begin
 ////            bp_axi_bid <= 0;
 ////            bp_axi_bresp <= 0;
 //            bp_axi_rvalid <= 0;
@@ -253,7 +251,7 @@ module MemorEDF #
 //            bp_axi_rdata <= 0;
 //            bp_axi_rresp <= 0;
 //            bp_axi_rlast <= 0;
-        end
+//        end
 //        else
 //        begin
 ////            bp_axi_bid <= m00_axi_bid;
@@ -265,7 +263,7 @@ module MemorEDF #
 //            bp_axi_rresp <= m00_axi_rresp;
 //            bp_axi_rlast <= m00_axi_rlast;
 //        end
-    end
+//    end
     
 //    assign m00_axi_rready = bp_axi_rready;
 ////    assign s00_axi_bid = bp_axi_bid;
@@ -350,63 +348,64 @@ module MemorEDF #
 		.stall(nonaxi_to_packetizer_stall)
 	);
 	
-//	ConfigurationPort # ( 
-//        .C_S_AXI_ID_WIDTH(C_S01_AXI_ID_WIDTH),
-//        .C_S_AXI_DATA_WIDTH(C_S01_AXI_DATA_WIDTH),
-//        .C_S_AXI_ADDR_WIDTH(C_S01_AXI_ADDR_WIDTH),
-//        .C_S_AXI_AWUSER_WIDTH(C_S01_AXI_AWUSER_WIDTH),
-//        .C_S_AXI_ARUSER_WIDTH(C_S01_AXI_ARUSER_WIDTH),
-//        .C_S_AXI_WUSER_WIDTH(C_S01_AXI_WUSER_WIDTH),
-//        .C_S_AXI_RUSER_WIDTH(C_S01_AXI_RUSER_WIDTH),
-//        .C_S_AXI_BUSER_WIDTH(C_S01_AXI_BUSER_WIDTH)
-//    ) configurationport (
-//        .S_AXI_ACLK(s01_axi_aclk),
-//        .S_AXI_ARESETN(s01_axi_aresetn),
-//        .S_AXI_AWID(s01_axi_awid),
-//        .S_AXI_AWADDR(s01_axi_awaddr),
-//        .S_AXI_AWLEN(s01_axi_awlen),
-//        .S_AXI_AWSIZE(s01_axi_awsize),
-//        .S_AXI_AWBURST(s01_axi_awburst),
-//        .S_AXI_AWLOCK(s01_axi_awlock),
-//        .S_AXI_AWCACHE(s01_axi_awcache),
-//        .S_AXI_AWPROT(s01_axi_awprot),
-//        .S_AXI_AWQOS(s01_axi_awqos),
-//        .S_AXI_AWREGION(s01_axi_awregion),
+	ConfigurationPort # ( 
+        .C_S_AXI_ID_WIDTH(C_S01_AXI_ID_WIDTH),
+        .C_S_AXI_DATA_WIDTH(C_S01_AXI_DATA_WIDTH),
+        .C_S_AXI_ADDR_WIDTH(C_S01_AXI_ADDR_WIDTH),
+        .C_S_AXI_AWUSER_WIDTH(C_S01_AXI_AWUSER_WIDTH),
+        .C_S_AXI_ARUSER_WIDTH(C_S01_AXI_ARUSER_WIDTH),
+        .C_S_AXI_WUSER_WIDTH(C_S01_AXI_WUSER_WIDTH),
+        .C_S_AXI_RUSER_WIDTH(C_S01_AXI_RUSER_WIDTH),
+        .C_S_AXI_BUSER_WIDTH(C_S01_AXI_BUSER_WIDTH)
+    ) configurationport (
+        .S_AXI_ACLK(s01_axi_aclk),
+        .S_AXI_ARESETN(s01_axi_aresetn),
+        .S_AXI_AWID(s01_axi_awid),
+        .S_AXI_AWADDR(s01_axi_awaddr),
+        .S_AXI_AWLEN(s01_axi_awlen),
+        .S_AXI_AWSIZE(s01_axi_awsize),
+        .S_AXI_AWBURST(s01_axi_awburst),
+        .S_AXI_AWLOCK(s01_axi_awlock),
+        .S_AXI_AWCACHE(s01_axi_awcache),
+        .S_AXI_AWPROT(s01_axi_awprot),
+        .S_AXI_AWQOS(s01_axi_awqos),
+        .S_AXI_AWREGION(s01_axi_awregion),
 //        .S_AXI_AWUSER(s01_axi_awuser),
-//        .S_AXI_AWVALID(s01_axi_awvalid),
-//        .S_AXI_AWREADY(s01_axi_awready),
-//        .S_AXI_WDATA(s01_axi_wdata),
-//        .S_AXI_WSTRB(s01_axi_wstrb),
-//        .S_AXI_WLAST(s01_axi_wlast),
+        .S_AXI_AWVALID(s01_axi_awvalid),
+        .S_AXI_AWREADY(s01_axi_awready),
+        .S_AXI_WDATA(s01_axi_wdata),
+        .S_AXI_WSTRB(s01_axi_wstrb),
+        .S_AXI_WLAST(s01_axi_wlast),
 //        .S_AXI_WUSER(s01_axi_wuser),
-//        .S_AXI_WVALID(s01_axi_wvalid),
-//        .S_AXI_WREADY(s01_axi_wready),
-//        .S_AXI_BID(s01_axi_bid),
-//        .S_AXI_BRESP(s01_axi_bresp),
+        .S_AXI_WVALID(s01_axi_wvalid),
+        .S_AXI_WREADY(s01_axi_wready),
+        .S_AXI_BID(s01_axi_bid),
+        .S_AXI_BRESP(s01_axi_bresp),
 //        .S_AXI_BUSER(s01_axi_buser),
-//        .S_AXI_BVALID(s01_axi_bvalid),
-//        .S_AXI_BREADY(s01_axi_bready),
-//        .S_AXI_ARID(s01_axi_arid),
-//        .S_AXI_ARADDR(s01_axi_araddr),
-//        .S_AXI_ARLEN(s01_axi_arlen),
-//        .S_AXI_ARSIZE(s01_axi_arsize),
-//        .S_AXI_ARBURST(s01_axi_arburst),
-//        .S_AXI_ARLOCK(s01_axi_arlock),
-//        .S_AXI_ARCACHE(s01_axi_arcache),
-//        .S_AXI_ARPROT(s01_axi_arprot),
-//        .S_AXI_ARQOS(s01_axi_arqos),
-//        .S_AXI_ARREGION(s01_axi_arregion),
+        .S_AXI_BVALID(s01_axi_bvalid),
+        .S_AXI_BREADY(s01_axi_bready),
+        .S_AXI_ARID(s01_axi_arid),
+        .S_AXI_ARADDR(s01_axi_araddr),
+        .S_AXI_ARLEN(s01_axi_arlen),
+        .S_AXI_ARSIZE(s01_axi_arsize),
+        .S_AXI_ARBURST(s01_axi_arburst),
+        .S_AXI_ARLOCK(s01_axi_arlock),
+        .S_AXI_ARCACHE(s01_axi_arcache),
+        .S_AXI_ARPROT(s01_axi_arprot),
+        .S_AXI_ARQOS(s01_axi_arqos),
+        .S_AXI_ARREGION(s01_axi_arregion),
 //        .S_AXI_ARUSER(s01_axi_aruser),
-//        .S_AXI_ARVALID(s01_axi_arvalid),
-//        .S_AXI_ARREADY(s01_axi_arready),
-//        .S_AXI_RID(s01_axi_rid),
-//        .S_AXI_RDATA(s01_axi_rdata),
-//        .S_AXI_RRESP(s01_axi_rresp),
-//        .S_AXI_RLAST(s01_axi_rlast),
+        .S_AXI_ARVALID(s01_axi_arvalid),
+        .S_AXI_ARREADY(s01_axi_arready),
+        .S_AXI_RID(s01_axi_rid),
+        .S_AXI_RDATA(s01_axi_rdata),
+        .S_AXI_RRESP(s01_axi_rresp),
+        .S_AXI_RLAST(s01_axi_rlast),
 //        .S_AXI_RUSER(s01_axi_ruser),
-//        .S_AXI_RVALID(s01_axi_rvalid),
-//        .S_AXI_RREADY(s01_axi_rready)
-//    );
+        .S_AXI_RVALID(s01_axi_rvalid),
+        .S_AXI_RREADY(s01_axi_rready),
+        .memory_out(buffers)
+    );
     
     // Instantiation of Axi Bus Interface M00_AXI
     Serializer # (
