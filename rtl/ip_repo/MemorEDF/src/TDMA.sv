eaea7bfd37ea9439f4b354b4e398d59bb36fb2f4
denishoornaert <denis.hoornaert@outlook.com>
2020-05-22 17:45:51 +0200

    Draft of the design. General version is commented

--- rtl/ip_repo/MemorEDF/src/TDMA.sv
+++ rtl/ip_repo/MemorEDF/src/TDMA.sv
@@ -27,35 +27,37 @@ module TDMA #(
     (
         clock,
         reset,
-        deltaT,
+        delta,
         selection
     );
 
     // Input definition
     input clock;
     input reset;
-    input [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] deltaT;
+    input [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] delta;
     
     // Output definition
     output [$clog2(NUMBER_OF_QUEUES)-1 : 0] selection;
     
     // Output registers
     reg [$clog2(NUMBER_OF_QUEUES)-1 : 0] internalSelection;
+    assign selection = internalSelection;
          
     // Definition of the internal register(s)
     reg [REGISTER_SIZE-1 : 0] counter;
-    reg [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] delta;
     
-    assign selection = internalSelection;
     
     wire [NUMBER_OF_QUEUES-1 : 0] [REGISTER_SIZE-1 : 0] sums;
     
-    genvar i;
-    for (i = 1; i < NUMBER_OF_QUEUES; i = i + 1)
-    begin
-        assign sums[i] = sums[i-1]+delta[i];
-    end
     assign sums[0] = delta[0];
+    assign sums[1] = sums[0]+delta[1];
+    assign sums[2] = sums[1]+delta[2];
+    assign sums[3] = sums[2]+delta[3];
+//    genvar i;
+//    for (i = 1; i < NUMBER_OF_QUEUES; i = i + 1)
+//    begin
+//        assign sums[i] = sums[i-1]+delta[i];
+//    end
     
     always @(posedge clock)
     begin
@@ -63,17 +65,9 @@ module TDMA #(
         begin
             internalSelection <= 0;
             counter <=  0;
-            for (int i = 0; i < NUMBER_OF_QUEUES; i = i + 1)
-            begin
-                delta[i] <= 0;
-            end 
         end
         else
-        begin 
-            for (int i = 0; i < NUMBER_OF_QUEUES; i = i + 1)
-            begin
-                delta[i] <= deltaT[i];
-            end
+        begin
             // Counter management if sum of the periods reached, then reset
             counter <= (counter >= sums[NUMBER_OF_QUEUES-1]-1)? 0 : counter+1;
             // Management of the selections depending on the periods
@@ -82,14 +76,30 @@ module TDMA #(
             begin
                 internalSelection <= 0;
             end
-            // Generation of the other cases
-            for (int i = 1; i < NUMBER_OF_QUEUES; i = i + 1)
+            else if(sums[0] <= counter & counter < sums[1])
             begin
-                if(sums[i-1] <= counter & counter < sums[i])
-                begin
-                    internalSelection <= i;
-                end
+                internalSelection <= 1;
+            end 
+            else if(sums[1] <= counter & counter < sums[2])
+            begin
+                internalSelection <= 2;
+            end 
+            else if(sums[2] <= counter & counter < sums[3])
+            begin
+                internalSelection <= 3;
             end
+            else
+            begin
+                internalSelection <= 0;
+            end
+//            // Generation of the other cases
+//            for (int i = 1; i < NUMBER_OF_QUEUES; i = i + 1)
+//            begin
+//                if(sums[i-1] <= counter & counter < sums[i])
+//                begin
+//                    internalSelection <= i;
+//                end
+//            end
         end 
     end
 
