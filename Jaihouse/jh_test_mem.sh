67ca517c7f6ed2201a0cccf8d62a54c981f22727
Shahin Roozkhosh <shahinroozkhosh@gmail.com>
2020-06-02 20:19:24 -0400

    JH

--- Jaihouse/jh_test_mem.sh
+++
@@ -1,8 +0,0 @@
-cd jailhouse_PL
-
-insmod driver/jailhouse.ko
-jailhouse enable configs/arm64/zynqmp-zcu102.cell
-jailhouse cell create configs/arm64/zynqmp-zcu102-gic-demo.cell
-jailhouse cell load 1 inmates/demos/arm64/mem-demo.bin
-
-
