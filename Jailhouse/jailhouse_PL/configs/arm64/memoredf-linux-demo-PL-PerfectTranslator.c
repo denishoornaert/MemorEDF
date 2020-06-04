8242bda157f1e1678ea41b0c7a48ffe5306fc171
Shahin Roozkhosh <shahinroozkhosh@gmail.com>
2020-06-04 19:09:04 -0400

    memedf PefectTranslator inmate

--- Jailhouse/jailhouse_PL/configs/arm64/memoredf-linux-demo-PL-PerfectTranslator.c
+++ Jailhouse/jailhouse_PL/configs/arm64/memoredf-linux-demo-PL-PerfectTranslator.c
@@ -21,7 +21,7 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[6];
+	struct jailhouse_memory mem_regions[5];
 	struct jailhouse_irqchip irqchips[1];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
