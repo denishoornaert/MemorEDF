003c6f00f434e3074421ced67762cfe7700aa768
Denis <denis.hoornaert@tum.de>
2020-12-21 10:36:11 +0100

    Loaded cell depends on jailhouse.config content

--- rootfs/common/linux-automated-dualport.sh
+++ rootfs/common/linux-automated-dualport.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+set -e
 
 source ~/common/jailhouse.config
+source ~/experiences/profile.config
+
+prefix=-col
+
+if [ "$mode" = "main-route" ]; then
+    prefix=-col-main-route
+elif [ "$mode" = "dual-slave" ]; then
+    prefix=-col-dual-slave-cached
+elif [ "$mode" = "simple-loop-back" ]; then
+    prefix=-col-simple-loop-back
+else
+    echo "No target matching! (Abort)"
+    exit 1
+fi
 
 cd $jh_path
 
-$jh_path/tools/jailhouse cell linux $jh_path/configs/arm64/schim-linux-inmate-col-dual-slave-cached.cell ../inmate_imgs/Image.inmate -d $jh_path/configs/arm64/dts/inmate-zynqmp-1c.dtb -i $img_path/rootfs.cpio.auto.sudo -c "console=ttyPS0,115200" -k13
+$jh_path/tools/jailhouse cell linux $jh_path/configs/arm64/schim-linux-inmate${prefix}.cell $img_path/Image.inmate -d $jh_path/configs/arm64/dts/inmate-zynqmp-1c.dtb -i $img_path/rootfs.cpio.auto.sudo -c "console=ttyPS0,115200" -k13
