61986358df052d1908e566a7a14c4ebe87bd070a
Denis <denis.hoornaert@tum.de>
2021-01-07 22:28:16 +0100

    New threshold bw benchmarks

---
+++ demo/src/threshold_bw_aging.c
@@ -0,0 +1,87 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "../include/config.h"
+#include "../include/tool.h"
+
+int main(int argc, char** argv) {
+
+    int lpd_fd  = open_fd();
+    int hpm_fd  = open_fd();
+
+    struct configuration* config = mmap((void*)0, LPD0_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED, lpd_fd, LPD0_ADDR);
+    unsigned* plim = mmap((void*)0, HPM0_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED|0x40, hpm_fd, HPM0_ADDR);
+
+    // Set the period registers (default: 0x002faf08)
+    (*config).periods[0] = 0x00000000;
+    (*config).periods[1] = 0x00000000;
+    (*config).periods[2] = 0x00000000;
+    (*config).periods[3] = 0x00000000;
+    // Set the deadline registers
+    (*config).deadlines[0] = 0x00000000;
+    (*config).deadlines[1] = 0x00000000;
+    (*config).deadlines[2] = 0x00000000;
+    (*config).deadlines[3] = 0x00000000;
+    // Set the priorities register
+    (*config).priorities = 0x00000000;
+    // Set the budget registers
+    (*config).budgets[0] = 0x00000000;
+    (*config).budgets[1] = 0x00000000;
+    (*config).budgets[2] = 0x00000000;
+    (*config).budgets[3] = 0x00000000;
+    // Set the hyper period register
+    (*config).hyperperiod = 0x00000000;
+    // Set the scheduler
+    (*config).scheduler = aging;
+
+    // Set the CPU
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+    sched_setaffinity(0, sizeof(mask), &mask);
+
+    printf("operation, threshold, sec, ns, MB, accesses\n");
+
+    unsigned k = 0;
+
+    for (unsigned threshold = 0; threshold < 9; threshold++) {
+        // Update thresholds
+        (*config).deadlines[0] = threshold;
+        (*config).deadlines[1] = threshold;
+        (*config).deadlines[2] = threshold;
+        (*config).deadlines[3] = threshold;
+
+        for (size_t j = 0; j < 100; j++) {
+            struct timespec time1, time2;
+            long unsigned sec, ns;
+
+            // read
+            clock_gettime(CLOCK_REALTIME, &time1);
+            for (unsigned i = ((0<<14)/sizeof(unsigned)); i < (HPM0_SIZE/sizeof(unsigned)); i+=(64*KB/sizeof(unsigned))) {
+                for (unsigned j = 0; j < (16*KB/sizeof(unsigned)); j+=(CACHE_LINE_SIZE/sizeof(unsigned))) {
+                    k = plim[i+j];
+                }
+            }
+            clock_gettime(CLOCK_REALTIME, &time2);
+
+            sec = diff(time1, time2).tv_sec;
+            ns  = diff(time1, time2).tv_nsec;
+            printf("read, %u, %lu, %lu, %u, %u\n", threshold, sec, ns, (HPM0_SIZE/4), ((HPM0_SIZE/4)/sizeof(u128)));
+        }
+    }
+
+    int unmap_result = 0;
+    unmap_result |= unmap(plim  , HPM0_SIZE);
+    unmap_result |= unmap(config, LPD0_SIZE);
+
+    return unmap_result;
+}
