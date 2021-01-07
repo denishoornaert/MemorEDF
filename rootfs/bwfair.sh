9eeb903bcdb6c8aacec077a2323ffb60cd5d3de7
Denis <denis.hoornaert@tum.de>
2021-01-07 22:28:34 +0100

    New scripts

---
+++ rootfs/bwfair.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+set -e
+
+source ~/common/jailhouse.config
+
+# Load experience variables
+cp $1 ~/experiences/profile.config
+source ~/experiences/profile.config
+
+# Load experience variables
+periods_1=(0 4 8 16 32)
+periods_2=(0 8 16 32 4)
+periods_3=(0 16 32 4 8)
+periods_4=(0 32 4 8 16)
+threshold_core_0=0
+threshold_core_1=0
+threshold_core_2=0
+threshold_core_3=0
+
+dest_dir="synthetic/bw"
+
+# Load the jailhouse root cell for SchIM
+if (lsmod | grep "jailhouse" &> /dev/null)
+then
+    :; # do nothing
+else
+    insmod $jh_path/driver/jailhouse.ko
+    jailhouse enable $jh_path/configs/arm64/schim-root.cell
+fi
+
+# Aging
+# Configure SchIM acording to the experiment profile
+~/common/config_schim_aging.out ${threshold_core_0} ${threshold_core_1} ${threshold_core_2} ${threshold_core_3}
+# Load colored Memory Bomb cells
+~/common/load_col_bombs.sh >> /dev/null
+# Start Memory Bombs in either Read or Write mode
+~/common/bombs_"$bombing_mode".sh >> /dev/null
+# Run synthetic bench with different level of contention
+for (( i = $((last_bomb+1)); i > 1; i-- )); do
+    # If not yet created, creates the destination directory
+    mkdir -p $dest_dir/"aging"
+    # Create output raw data file
+    touch ${dest_dir}/"aging"/${i}_cores.csv
+    # Generate and redirect data
+    echo "Start - aging - core ${i}"
+    ~/synthetic/threshold_bw_aging.out > ${dest_dir}/"aging"/${i}_cores.csv
+    # Kill one cell
+    jailhouse cell destroy $((i-1)) >> /dev/null
+done
+# Run alone
+# Create output raw data file
+touch ${dest_dir}/"aging"/1_cores.csv
+# Generate and redirect data
+echo "Start - aging - core ${i}"
+~/synthetic/threshold_bw_aging.out > ${dest_dir}/"aging"/1_cores.csv
+
+
+# FIBO
+# Configure SchIM acording to the experiment profile
+~/common/config_schim_fibo.out ${threshold_core_0} ${threshold_core_1} ${threshold_core_2} ${threshold_core_3}
+# Load colored Memory Bomb cells
+~/common/load_col_bombs.sh >> /dev/null
+# Start Memory Bombs in either Read or Write mode
+~/common/bombs_"$bombing_mode".sh >> /dev/null
+# Run synthetic bench with different level of contention
+for (( i = $((last_bomb+1)); i > 1; i-- )); do
+    # If not yet created, creates the destination directory
+    mkdir -p $dest_dir/"fibo"
+    # Create output raw data file
+    touch ${dest_dir}/"fibo"/${i}_cores.csv
+    # Generate and redirect data
+    echo "Start - fibo - core ${i}"
+    ~/synthetic/threshold_bw_fibo.out > ${dest_dir}/"fibo"/${i}_cores.csv
+    # Kill one cell
+    jailhouse cell destroy $((i-1)) >> /dev/null
+done
+# Run alone
+# Create output raw data file
+touch ${dest_dir}/"fibo"/1_cores.csv
+# Generate and redirect data
+echo "Start - fibo - core ${i}"
+~/synthetic/threshold_bw_fibo.out > ${dest_dir}/"fibo"/1_cores.csv
+
+
+#GALLOIS
+# Configure SchIM acording to the experiment profile
+~/common/config_schim_gallois.out ${threshold_core_0} ${threshold_core_1} ${threshold_core_2} ${threshold_core_3}
+# Load colored Memory Bomb cells
+~/common/load_col_bombs.sh >> /dev/null
+# Start Memory Bombs in either Read or Write mode
+~/common/bombs_"$bombing_mode".sh >> /dev/null
+# Run synthetic bench with different level of contention
+for (( i = $((last_bomb+1)); i > 1; i-- )); do
+    # If not yet created, creates the destination directory
+    mkdir -p $dest_dir/"gallois"
+    # Create output raw data file
+    touch ${dest_dir}/"gallois"/${i}_cores.csv
+    # Generate and redirect data
+    echo "Start - gallois - core ${i}"
+    ~/synthetic/threshold_bw_gallois.out > ${dest_dir}/"gallois"/${i}_cores.csv
+    # Kill one cell
+    jailhouse cell destroy $((i-1)) >> /dev/null
+done
+# Run alone
+# Create output raw data file
+touch ${dest_dir}/"gallois"/1_cores.csv
+# Generate and redirect data
+echo "Start - gallois - core ${i}"
+~/synthetic/threshold_bw_gallois.out > ${dest_dir}/"gallois"/1_cores.csv
