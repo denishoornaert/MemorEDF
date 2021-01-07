0f04c0e63d9a96a2279428afd9cb31480088ff39
Denis <denis.hoornaert@tum.de>
2021-01-07 22:29:12 +0100

    Newly gather raw data from the aging, fibo, gallois and tdma experiments

--- script/threshold_bw/threshold.py
+++ script/threshold_bw/threshold.py
@@ -18,6 +18,7 @@ def read(filepath, skip):
 linestyles = ['-', '--', '-.', ':']
 coord = [(0, 0), (0, 1), (1, 0), (1, 1)]
 mits = [4, 8, 16, 32]
+periods = [4, 8, 16, 32]
 priorities_labels = ["Lowest", "Third highest", "Second highest", "Highest"]
 
 #if (__name__ == '__main__'):
@@ -154,14 +155,182 @@ priorities_labels = ["Lowest", "Third highest", "Second highest", "Highest"]
 #
 #    plt.savefig("threshold.png")
 
+#if (__name__ == '__main__'):
+#    fig, axs = plt.subplots(2, 2, figsize=(10.5, 8))
+#
+#    for core in range(1, 5, 1):
+#        axs[coord[core-1][0], coord[core-1][1]].set_title("Contention level: "+str(core)+" core"+("s" if (core > 1) else " (Solo)"))
+#        axs[coord[core-1][0], coord[core-1][1]].set_ylim([0, 700])
+#        for mit in range(1, 5, 1):
+#            data = read("mit_"+str(mit)+"/"+str(core)+"_cores.csv", 1)
+#
+#            x = list(data.keys())
+#            x.sort()
+#
+#            for key in x:
+#                data[key] = {"avg":np.mean(data[key]), "std":np.std(data[key])}
+#
+#            y = [data[key]["avg"] for key in x]
+#            e = [data[key]["std"] for key in x]
+#            y = y[1:]+[y[0]]
+#            e = e[1:]+[e[0]]
+#
+#            axs[coord[core-1][0], coord[core-1][1]].errorbar(x, y, yerr=e, label="MIT = "+str(mits[mit-1]))
+#
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticks(x)
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
+#
+#    for ax in axs.flat:
+#        ax.set(xlabel="Threshold", ylabel="Bandwidth (MBps)")
+#
+#    # Hide x labels and tick labels for top plots and y ticks for right plots.
+#    for ax in axs.flat:
+#        ax.label_outer()
+#
+#    #axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2))#bbox_to_anchor=(1.05, 1), loc='upper left')
+#
+#    fig.suptitle("Using TS, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
+#
+#    handles, labels = axs[1, 1].get_legend_handles_labels()
+#    fig.legend(handles, labels, loc='lower center', ncol=5,)#, bbox_to_anchor=(0.0, -0.2))
+#
+#    fig.savefig("threshold_bw.pdf", dpi=300)
+#    fig.savefig("threshold_bw.png", dpi=300)
+
+#if (__name__ == '__main__'):
+#    fig, axs = plt.subplots(2, 2, figsize=(10.5, 8))
+#
+#    for core in range(1, 5, 1):
+#        axs[coord[core-1][0], coord[core-1][1]].set_title("Contention level: "+str(core)+" core"+("s" if (core > 1) else " (Solo)"))
+#        axs[coord[core-1][0], coord[core-1][1]].set_ylim([0, 700])
+#        for period in range(1, 5, 1):
+#            data = read("period_"+str(period)+"/"+str(core)+"_cores.csv", 1)
+#
+#            x = list(data.keys())
+#            x.sort()
+#
+#            for key in x:
+#                data[key] = {"avg":np.mean(data[key]), "std":np.std(data[key])}
+#
+#            y = [data[key]["avg"] for key in x]
+#            e = [data[key]["std"] for key in x]
+#            y = y[1:]+[y[0]]
+#            e = e[1:]+[e[0]]
+#
+#            axs[coord[core-1][0], coord[core-1][1]].errorbar(x, y, yerr=e, label="Period = "+str(periods[period-1]))
+#
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticks(x)
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
+#
+#    for ax in axs.flat:
+#        ax.set(xlabel="Threshold", ylabel="Bandwidth (MBps)")
+#
+#    # Hide x labels and tick labels for top plots and y ticks for right plots.
+#    for ax in axs.flat:
+#        ax.label_outer()
+#
+#    #axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2))#bbox_to_anchor=(1.05, 1), loc='upper left')
+#
+#    fig.suptitle("Using TDMA, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
+#
+#    handles, labels = axs[1, 1].get_legend_handles_labels()
+#    fig.legend(handles, labels, loc='lower center', ncol=5,)#, bbox_to_anchor=(0.0, -0.2))
+#
+#    fig.savefig("threshold_tdma.pdf", dpi=300)
+#    fig.savefig("threshold_tdma.png", dpi=300)
+
+#if (__name__ == '__main__'):
+#    fig, axs = plt.subplots(2, 2, figsize=(10.5, 8))
+#
+#    for period in range(1, 5, 1):
+#        axs[coord[period-1][0], coord[period-1][1]].set_title("CUA period = "+str(periods[period-1]))
+#        axs[coord[period-1][0], coord[period-1][1]].set_ylim([0, 700])
+#        for core in range(1, 5, 1):
+#            data = read("period_"+str(period)+"/"+str(core)+"_cores.csv", 1)
+#
+#            x = list(data.keys())
+#            x.sort()
+#
+#            for key in x:
+#                data[key] = {"avg":np.mean(data[key]), "std":np.std(data[key])}
+#
+#            y = [data[key]["avg"] for key in x]
+#            e = [data[key]["std"] for key in x]
+#            y = y[1:]+[y[0]]
+#            e = e[1:]+[e[0]]
+#
+#            axs[coord[period-1][0], coord[period-1][1]].errorbar(x, y, yerr=e, label="Cores = "+str(core))
+#
+#            axs[coord[period-1][0], coord[period-1][1]].set_xticks(x)
+#            axs[coord[period-1][0], coord[period-1][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
+#
+#    for ax in axs.flat:
+#        ax.set(xlabel="Threshold", ylabel="Bandwidth (MBps)")
+#
+#    # Hide x labels and tick labels for top plots and y ticks for right plots.
+#    for ax in axs.flat:
+#        ax.label_outer()
+#
+#    #axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2))#bbox_to_anchor=(1.05, 1), loc='upper left')
+#
+#    fig.suptitle("Using TDMA, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
+#
+#    handles, labels = axs[1, 1].get_legend_handles_labels()
+#    fig.legend(handles, labels, loc='lower center', ncol=5,)#, bbox_to_anchor=(0.0, -0.2))
+#
+#    fig.savefig("threshold_tdma_rev.pdf", dpi=300)
+#    fig.savefig("threshold_tdma_rev.png", dpi=300)
+
+#if (__name__ == '__main__'):
+#    fig, axs = plt.subplots(2, 2, figsize=(10.5, 8))
+#
+#    for core in range(1, 5, 1):
+#        axs[coord[core-1][0], coord[core-1][1]].set_title("Contention level: "+str(core)+" core"+("s" if (core > 1) else " (Solo)"))
+#        axs[coord[core-1][0], coord[core-1][1]].set_ylim([0, 700])
+#        for policy in ["aging", "fibo", "gallois"]:
+#            data = read(policy+"/"+str(core)+"_cores.csv", 1)
+#
+#            x = list(data.keys())
+#            x.sort()
+#
+#            for key in x:
+#                data[key] = {"avg":np.mean(data[key]), "std":np.std(data[key])}
+#
+#            y = [data[key]["avg"] for key in x]
+#            e = [data[key]["std"] for key in x]
+#            y = y[1:]+[y[0]]
+#            e = e[1:]+[e[0]]
+#
+#            axs[coord[core-1][0], coord[core-1][1]].errorbar(x, y, yerr=e, label=policy)
+#
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticks(x)
+#            axs[coord[core-1][0], coord[core-1][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
+#
+#    for ax in axs.flat:
+#        ax.set(xlabel="Threshold", ylabel="Bandwidth (MBps)")
+#
+#    # Hide x labels and tick labels for top plots and y ticks for right plots.
+#    for ax in axs.flat:
+#        ax.label_outer()
+#
+#    #axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2))#bbox_to_anchor=(1.05, 1), loc='upper left')
+#
+#    fig.suptitle("Using Aging, Fibonacci and Gallois, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
+#
+#    handles, labels = axs[1, 1].get_legend_handles_labels()
+#    fig.legend(handles, labels, loc='lower center', ncol=5,)#, bbox_to_anchor=(0.0, -0.2))
+#
+#    fig.savefig("threshold_fair.pdf", dpi=300)
+#    fig.savefig("threshold_fair.png", dpi=300)
+
 if (__name__ == '__main__'):
     fig, axs = plt.subplots(2, 2, figsize=(10.5, 8))
 
-    for core in range(1, 5, 1):
-        axs[coord[core-1][0], coord[core-1][1]].set_title("Contention level: "+str(core)+" core"+("s" if (core > 1) else " (Solo)"))
-        axs[coord[core-1][0], coord[core-1][1]].set_ylim([0, 700])
-        for mit in range(1, 5, 1):
-            data = read("mit_"+str(mit)+"/"+str(core)+"_cores.csv", 1)
+    for index, policy in enumerate(["aging", "fibo", "gallois"]):
+        axs[coord[index][0], coord[index][1]].set_title(policy)
+        axs[coord[index][0], coord[index][1]].set_ylim([0, 700])
+        for core in range(1, 5, 1):
+            data = read(policy+"/"+str(core)+"_cores.csv", 1)
 
             x = list(data.keys())
             x.sort()
@@ -174,10 +343,10 @@ if (__name__ == '__main__'):
             y = y[1:]+[y[0]]
             e = e[1:]+[e[0]]
 
-            axs[coord[core-1][0], coord[core-1][1]].errorbar(x, y, yerr=e, label="MIT = "+str(mits[mit-1]))
+            axs[coord[index][0], coord[index][1]].errorbar(x, y, yerr=e, label="Cores = "+str(core))
 
-            axs[coord[core-1][0], coord[core-1][1]].set_xticks(x)
-            axs[coord[core-1][0], coord[core-1][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
+            axs[coord[index][0], coord[index][1]].set_xticks(x)
+            axs[coord[index][0], coord[index][1]].set_xticklabels([str(x[j]) for j in range(1,9)]+["Disabled"])
 
     for ax in axs.flat:
         ax.set(xlabel="Threshold", ylabel="Bandwidth (MBps)")
@@ -188,10 +357,10 @@ if (__name__ == '__main__'):
 
     #axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2))#bbox_to_anchor=(1.05, 1), loc='upper left')
 
-    fig.suptitle("Using TS, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
+    fig.suptitle("Using Aging, Fibonacci and Gallois, bandwidth experienced by the cores for different levels\nof contention and thresholds", fontsize=16)
 
-    handles, labels = axs[1, 1].get_legend_handles_labels()
+    handles, labels = axs[0, 0].get_legend_handles_labels()
     fig.legend(handles, labels, loc='lower center', ncol=5,)#, bbox_to_anchor=(0.0, -0.2))
 
-    fig.savefig("threshold_bw.pdf", dpi=300)
-    fig.savefig("threshold_bw.png", dpi=300)
+    fig.savefig("threshold_fair_rev.pdf", dpi=300)
+    fig.savefig("threshold_fair_rev.png", dpi=300)
