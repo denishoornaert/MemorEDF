0babf16f010427d8cfd93b7388925b4be55de0e7
Denis <denis.hoornaert@tum.de>
2021-02-11 15:56:49 +0100

    Update raw-data path in SD-card

--- retrieve_data.sh
+++ retrieve_data.sh
@@ -39,77 +39,77 @@ sudo cp ${path_to_rootfs}/home/root/synthetic/SchIM_Gallois/3_cores.csv script/b
 sudo cp ${path_to_rootfs}/home/root/synthetic/SchIM_Gallois/2_cores.csv script/bw_comparisons/SchIM_Gallois/
 sudo cp ${path_to_rootfs}/home/root/synthetic/SchIM_Gallois/1_cores.csv script/bw_comparisons/SchIM_Gallois/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_1/1_cores.csv script/threshold_bw/prio_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_1/2_cores.csv script/threshold_bw/prio_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_1/3_cores.csv script/threshold_bw/prio_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_1/4_cores.csv script/threshold_bw/prio_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_1/1_cores.csv script/threshold_bw/prio_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_1/2_cores.csv script/threshold_bw/prio_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_1/3_cores.csv script/threshold_bw/prio_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_1/4_cores.csv script/threshold_bw/prio_1/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_2/1_cores.csv script/threshold_bw/prio_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_2/2_cores.csv script/threshold_bw/prio_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_2/3_cores.csv script/threshold_bw/prio_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_2/4_cores.csv script/threshold_bw/prio_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_2/1_cores.csv script/threshold_bw/prio_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_2/2_cores.csv script/threshold_bw/prio_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_2/3_cores.csv script/threshold_bw/prio_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_2/4_cores.csv script/threshold_bw/prio_2/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_3/1_cores.csv script/threshold_bw/prio_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_3/2_cores.csv script/threshold_bw/prio_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_3/3_cores.csv script/threshold_bw/prio_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_3/4_cores.csv script/threshold_bw/prio_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_3/1_cores.csv script/threshold_bw/prio_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_3/2_cores.csv script/threshold_bw/prio_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_3/3_cores.csv script/threshold_bw/prio_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_3/4_cores.csv script/threshold_bw/prio_3/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_4/1_cores.csv script/threshold_bw/prio_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_4/2_cores.csv script/threshold_bw/prio_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_4/3_cores.csv script/threshold_bw/prio_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/prio_4/4_cores.csv script/threshold_bw/prio_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_4/1_cores.csv script/threshold_bw/prio_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_4/2_cores.csv script/threshold_bw/prio_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_4/3_cores.csv script/threshold_bw/prio_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/prio_4/4_cores.csv script/threshold_bw/prio_4/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_1/1_cores.csv script/threshold_bw/mit_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_1/2_cores.csv script/threshold_bw/mit_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_1/3_cores.csv script/threshold_bw/mit_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_1/4_cores.csv script/threshold_bw/mit_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_1/1_cores.csv script/threshold_bw/mit_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_1/2_cores.csv script/threshold_bw/mit_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_1/3_cores.csv script/threshold_bw/mit_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_1/4_cores.csv script/threshold_bw/mit_1/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_2/1_cores.csv script/threshold_bw/mit_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_2/2_cores.csv script/threshold_bw/mit_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_2/3_cores.csv script/threshold_bw/mit_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_2/4_cores.csv script/threshold_bw/mit_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_2/1_cores.csv script/threshold_bw/mit_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_2/2_cores.csv script/threshold_bw/mit_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_2/3_cores.csv script/threshold_bw/mit_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_2/4_cores.csv script/threshold_bw/mit_2/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_3/1_cores.csv script/threshold_bw/mit_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_3/2_cores.csv script/threshold_bw/mit_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_3/3_cores.csv script/threshold_bw/mit_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_3/4_cores.csv script/threshold_bw/mit_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_3/1_cores.csv script/threshold_bw/mit_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_3/2_cores.csv script/threshold_bw/mit_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_3/3_cores.csv script/threshold_bw/mit_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_3/4_cores.csv script/threshold_bw/mit_3/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_4/1_cores.csv script/threshold_bw/mit_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_4/2_cores.csv script/threshold_bw/mit_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_4/3_cores.csv script/threshold_bw/mit_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/mit_4/4_cores.csv script/threshold_bw/mit_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_4/1_cores.csv script/threshold_bw/mit_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_4/2_cores.csv script/threshold_bw/mit_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_4/3_cores.csv script/threshold_bw/mit_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/mit_4/4_cores.csv script/threshold_bw/mit_4/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_1/1_cores.csv script/threshold_bw/period_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_1/2_cores.csv script/threshold_bw/period_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_1/3_cores.csv script/threshold_bw/period_1/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_1/4_cores.csv script/threshold_bw/period_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_1/1_cores.csv script/threshold_bw/period_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_1/2_cores.csv script/threshold_bw/period_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_1/3_cores.csv script/threshold_bw/period_1/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_1/4_cores.csv script/threshold_bw/period_1/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_2/1_cores.csv script/threshold_bw/period_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_2/2_cores.csv script/threshold_bw/period_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_2/3_cores.csv script/threshold_bw/period_2/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_2/4_cores.csv script/threshold_bw/period_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_2/1_cores.csv script/threshold_bw/period_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_2/2_cores.csv script/threshold_bw/period_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_2/3_cores.csv script/threshold_bw/period_2/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_2/4_cores.csv script/threshold_bw/period_2/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_3/1_cores.csv script/threshold_bw/period_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_3/2_cores.csv script/threshold_bw/period_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_3/3_cores.csv script/threshold_bw/period_3/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_3/4_cores.csv script/threshold_bw/period_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_3/1_cores.csv script/threshold_bw/period_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_3/2_cores.csv script/threshold_bw/period_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_3/3_cores.csv script/threshold_bw/period_3/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_3/4_cores.csv script/threshold_bw/period_3/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_4/1_cores.csv script/threshold_bw/period_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_4/2_cores.csv script/threshold_bw/period_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_4/3_cores.csv script/threshold_bw/period_4/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/period_4/4_cores.csv script/threshold_bw/period_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_4/1_cores.csv script/threshold_bw/period_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_4/2_cores.csv script/threshold_bw/period_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_4/3_cores.csv script/threshold_bw/period_4/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/period_4/4_cores.csv script/threshold_bw/period_4/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/aging/1_cores.csv script/threshold_bw/aging/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/aging/2_cores.csv script/threshold_bw/aging/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/aging/3_cores.csv script/threshold_bw/aging/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/aging/4_cores.csv script/threshold_bw/aging/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/aging/1_cores.csv script/threshold_bw/aging/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/aging/2_cores.csv script/threshold_bw/aging/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/aging/3_cores.csv script/threshold_bw/aging/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/aging/4_cores.csv script/threshold_bw/aging/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/fibo/1_cores.csv script/threshold_bw/fibo/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/fibo/2_cores.csv script/threshold_bw/fibo/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/fibo/3_cores.csv script/threshold_bw/fibo/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/fibo/4_cores.csv script/threshold_bw/fibo/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/fibo/1_cores.csv script/threshold_bw/fibo/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/fibo/2_cores.csv script/threshold_bw/fibo/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/fibo/3_cores.csv script/threshold_bw/fibo/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/fibo/4_cores.csv script/threshold_bw/fibo/
 
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/gallois/1_cores.csv script/threshold_bw/gallois/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/gallois/2_cores.csv script/threshold_bw/gallois/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/gallois/3_cores.csv script/threshold_bw/gallois/
-sudo cp ${path_to_rootfs}/home/root/synthetic/bw/gallois/4_cores.csv script/threshold_bw/gallois/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/gallois/1_cores.csv script/threshold_bw/gallois/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/gallois/2_cores.csv script/threshold_bw/gallois/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/gallois/3_cores.csv script/threshold_bw/gallois/
+sudo cp ${path_to_rootfs}/home/root/threshold/results/gallois/4_cores.csv script/threshold_bw/gallois/
