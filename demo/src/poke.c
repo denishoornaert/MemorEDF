3bc3fa58ec03db6576a8139f81fe1532308773d7
Denis <denis.hoornaert@tum.de>
2021-02-20 01:19:26 +0100

    Enable WRAP transaction creation

--- demo/src/poke.c
+++ demo/src/poke.c
@@ -12,6 +12,9 @@
 #include "../include/config.h"
 #include "../include/tool.h"
 
+#define PAGE_SIZE 4*1024
+#define PAGE_SHIFT 12
+#define PAGE_MASK 0x0000000000000fff
 
 int main(int argc, char** argv) {
     char mode;
@@ -26,7 +29,7 @@ int main(int argc, char** argv) {
         sscanf(argv[4], "%x", &data);
     }
 
-    printf("mode = %c, cpu = %d, addr = %x\n", mode, cpu, addr);
+    printf("mode = %c, cpu = %d, addr = %lx\n", mode, cpu, addr);
 
     cpu_set_t mask;
     CPU_ZERO(&mask);
@@ -36,47 +39,51 @@ int main(int argc, char** argv) {
     int lpd_fd  = open_fd();
     int hpm_fd  = open_fd();
 
-    struct configuration* config = mmap((void*)0, LPD0_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED, lpd_fd, LPD0_ADDR);
-    unsigned* plim = mmap((void*)0, HPM0_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED|0x40, hpm_fd, addr);
+    struct configuration* config;// = mmap((void*)0, LPD0_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED, lpd_fd, LPD0_ADDR);
+    unsigned* plim = mmap((void*)0, PAGE_SIZE, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED|0x40, hpm_fd, ((~PAGE_MASK)&addr));
 
     printf("After mmaps\n");
 
-    // FP chosen so that, whatever the core, transactions are served.
-    // Set the period registers (default: 0x002faf08)
-    (*config).periods[0] = 0x00000000;
-    (*config).periods[1] = 0x00000000;
-    (*config).periods[2] = 0x00000000;
-    (*config).periods[3] = 0x00000000;
-    // Set the deadline registers
-    (*config).deadlines[0] = 0x00000000;
-    (*config).deadlines[1] = 0x00000000;
-    (*config).deadlines[2] = 0x00000000;
-    (*config).deadlines[3] = 0x00000000;
-    // Set the priorities register
-    (*config).priorities = 0x0f0e0d0c;
-    // Set the budget registers
-    (*config).budgets[0] = 0x00000000;
-    (*config).budgets[1] = 0x00000000;
-    (*config).budgets[2] = 0x00000000;
-    (*config).budgets[3] = 0x00000000;
-    // Set the hyper period register
-    (*config).hyperperiod = 0x00000000;
-    // Set the scheduler
-    (*config).scheduler = fp;
+//    // FP chosen so that, whatever the core, transactions are served.
+//    // Set the period registers (default: 0x002faf08)
+//    (*config).periods[0] = 0x00000000;
+//    (*config).periods[1] = 0x00000000;
+//    (*config).periods[2] = 0x00000000;
+//    (*config).periods[3] = 0x00000000;
+//    // Set the deadline registers
+//    (*config).deadlines[0] = 0x00000000;
+//    (*config).deadlines[1] = 0x00000000;
+//    (*config).deadlines[2] = 0x00000000;
+//    (*config).deadlines[3] = 0x00000000;
+//    // Set the priorities register
+//    (*config).priorities = 0x0f0e0d0c;
+//    // Set the budget registers
+//    (*config).budgets[0] = 0x00000000;
+
+//    (*config).budgets[1] = 0x00000000;
+//    (*config).budgets[2] = 0x00000000;
+//    (*config).budgets[3] = 0x00000000;
+//    // Set the hyper period register
+//    (*config).hyperperiod = 0x00000000;
+//    // Set the scheduler
+//    (*config).scheduler = fp;
 
     printf("After SchIm config\n");
 
+    long unsigned target = (PAGE_MASK & addr)/sizeof(unsigned);
+    printf("addr = %lx\n", ((~PAGE_MASK) & addr));
+    printf("target = %lx\n", target);
     // Write
     if(mode == 'w') {
-        plim[0] = data;
+        plim[target] = data;
     }
     else if(mode == 'r') {
-        printf("%x\n", plim[0]);
+        printf("%x\n", plim[target]);
     }
 
     int unmap_result = 0;
-    unmap_result |= unmap(plim  , HPM0_SIZE);
-    unmap_result |= unmap(config, LPD0_SIZE);
+    unmap_result |= unmap(plim  , PAGE_SIZE);
+    //unmap_result |= unmap(config, LPD0_SIZE);
 
     return unmap_result;
 }
