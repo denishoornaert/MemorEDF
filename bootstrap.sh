90da93f3c4445ac2a997298ec4b3f14e3283038c
Denis <denis.hoornaert@tum.de>
2021-02-27 12:33:08 +0100

    Remove sd-card root partition mounting as already mounted by default and umount both sd-card partitions in /run/media/

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,43 +1,33 @@
-mkdir -p /mnt/sd/
-
-mount /dev/mmcblk0p2 /mnt/sd/
-
-cp -r /mnt/sd/home/root/benchmarks/ ~/
-cp -r /mnt/sd/home/root/brainfreeze/ ~/
-cp -r /mnt/sd/home/root/common/ ~/
-cp -r /mnt/sd/home/root/experiences/ ~/
-cp -r /mnt/sd/home/root/synthetic/ ~/
-cp -r /mnt/sd/home/root/threshold/ ~/
-mkdir -p ~/jailhouse-rt/hypervisor/
-cp -r /mnt/sd/home/root/jailhouse-rt/hypervisor/jailhouse.bin ~/jailhouse-rt/hypervisor/
-mkdir -p ~/jailhouse-rt/driver/
-cp -r /mnt/sd/home/root/jailhouse-rt/driver/jailhouse.ko ~/jailhouse-rt/driver/
-mkdir -p ~/jailhouse-rt/tools/
-cp -r /mnt/sd/home/root/jailhouse-rt/tools/jailhouse ~/jailhouse-rt/tools/
-mkdir -p ~/jailhouse-rt/inmates/demos/arm64/
-cp -r /mnt/sd/home/root/jailhouse-rt/inmates/demos/arm64/mem-bomb.bin ~/jailhouse-rt/inmates/demos/arm64/
-mkdir -p ~/jailhouse-rt/configs/arm64/
-cp -r /mnt/sd/home/root/jailhouse-rt/configs/arm64/schim-rootcol-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
-cp -r /mnt/sd/home/root/jailhouse-rt/configs/arm64/schim-bomb1-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
-cp -r /mnt/sd/home/root/jailhouse-rt/configs/arm64/schim-bomb2-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
-cp -r /mnt/sd/home/root/jailhouse-rt/configs/arm64/schim-bomb3-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
-
-umount /mnt/sd/
-
+cp -r /run/media/mmcblk0p2/home/root/benchmarks/ ~/
+cp -r /run/media/mmcblk0p2/home/root/brainfreeze/ ~/
+cp -r /run/media/mmcblk0p2/home/root/common/ ~/
+cp -r /run/media/mmcblk0p2/home/root/experiences/ ~/
+cp -r /run/media/mmcblk0p2/home/root/synthetic/ ~/
+cp -r /run/media/mmcblk0p2/home/root/threshold/ ~/
 mkdir -p /lib/firmware/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/hypervisor/jailhouse.bin /lib/firmware/
+mkdir -p ~/jailhouse-rt/driver/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/driver/jailhouse.ko ~/jailhouse-rt/driver/
+mkdir -p ~/jailhouse-rt/tools/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/tools/jailhouse ~/jailhouse-rt/tools/
+mkdir -p ~/jailhouse-rt/inmates/demos/arm64/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/inmates/demos/arm64/mem-bomb.bin ~/jailhouse-rt/inmates/demos/arm64/
+mkdir -p ~/jailhouse-rt/configs/arm64/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/configs/arm64/schim-rootcol-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/configs/arm64/schim-bomb1-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/configs/arm64/schim-bomb2-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
+cp -r /run/media/mmcblk0p2/home/root/jailhouse-rt/configs/arm64/schim-bomb3-col-dual-slave-cached.cell  ~/jailhouse-rt/configs/arm64/
 
-cp ~/jailhouse-rt/hypervisor/jailhouse.bin /lib/firmware/
+umount /run/media/mmcblk0p1/
+umount /run/media/mmcblk0p2/
 
 cd jailhouse-rt/
 
 insmod driver/jailhouse.ko
 
 ./tools/jailhouse enable configs/arm64/schim-rootcol-dual-slave-cached.cell
-
 ./tools/jailhouse cell list
 
 source common/jailhouse.config
-
 cp ~/experiences/synthetic/fp_schim.config ~/experiences/profile.config
-
 ./common/load_col_bombs.sh
