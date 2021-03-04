b32c392499e01f819275daefb2bee1d166d52713
Denis <denis.hoornaert@tum.de>
2021-03-04 01:44:12 +0100

    Reorder instruction sequence

--- rootfs/launch_benchmarks.sh
+++ rootfs/launch_benchmarks.sh
@@ -16,17 +16,22 @@ source ~/experiences/profile.config
 # Creat dump folder
 mkdir -p ~/${dest_dir}/${mode}/${scheduling_policy}
 
+# Start the fake Memory Bomb
+touch ~/${dest_dir}/${mode}/${scheduling_policy}/sdvbs_stress.csv
+current_path=`pwd`
+cd ~/benchmarks/root-cell/
+
 post_fix=""
 # Configure SchIM acording to the experiment profile
 if [ "$scheduling_policy" = "fp" ]; then
+    echo "${scheduling_policy}_${priorities}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
     ~/common/config_schim_"$scheduling_policy".out $priorities $threshold_core_0 $threshold_core_1 $threshold_core_2 $threshold_core_3
-    post_fix="${scheduling_policy}_${priorities}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
 elif [ "$scheduling_policy" = "tdma" ]; then
+    echo "${scheduling_policy}_${period_core_0}_${period_core_1}_${period_core_2}_${period_core_3}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
     ~/common/config_schim_"$scheduling_policy".out $period_core_0 $period_core_1 $period_core_2 $period_core_3 $threshold_core_0 $threshold_core_1 $threshold_core_2 $threshold_core_3
-    post_fix="${scheduling_policy}_${period_core_0}_${period_core_1}_${period_core_2}_${period_core_3}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
 elif [ "$scheduling_policy" = "ts" ]; then
+    echo "${scheduling_policy}_${mit_core_0}_${mit_core_1}_${mit_core_2}_${mit_core_3}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
     ~/common/config_schim_"$scheduling_policy".out $mit_core_0 $mit_core_1 $mit_core_2 $mit_core_3 $threshold_core_0 $threshold_core_1 $threshold_core_2 $threshold_core_3
-    post_fix="${scheduling_policy}_${mit_core_0}_${mit_core_1}_${mit_core_2}_${mit_core_3}_${threshold_core_0}_${threshold_core_1}_${threshold_core_2}_${threshold_core_3}"
 elif [ "$scheduling_policy" = "NA" ]; then
     : # No policy is available: Do nothing
 elif [ "$scheduling_policy" = "fibo" ]; then
@@ -43,10 +48,6 @@ else
     exit 1
 fi
 
-# Start the fake Memory Bomb
-touch ~/${dest_dir}/${mode}/${scheduling_policy}/sdvbs_stress.csv
-current_path=`pwd`
-cd ~/benchmarks/root-cell/
 bash run_all_sdvbs.sh ~/${dest_dir}/${mode}/${scheduling_policy}/sdvbs_stress.csv
 cd ${current_path}
 
